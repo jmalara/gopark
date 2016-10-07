ec8da7265dbef5d2d7aacda6479cf5c4fde3fc03
Jeremy Malara <jmalara@beachbody.com>
2016-10-06 22:16:15 -0700

    adding stuff

--- static/js/get_counts.js
+++ static/js/get_counts.js
@@ -1,9 +1,13 @@
-{% raw %}
+▽
 angular.module('get_counts', [])
 .controller('car_counts', function($scope, $http) {
+  $scope.getData = function(){
     $http.get('http://gpy.io/apizone').
         then(function(response) {
             $scope.car_counts = response.data;
         });
+  };
+
+  // Run function every second
+  setInterval($scope.getData, 1000);
 });
-{% endraw %}
